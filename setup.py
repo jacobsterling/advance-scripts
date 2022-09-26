48d71773f558f31a7fa867e6183676a2bbe118de
quikc <j10.sterling@gmail.com>
2022-09-26 15:17:28 +0100

    dd

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "advancePythonScripts",
-    version="0.2",
+    version="0.3",
     description="Python data analysis scripts",
     author="Jacob Sterling",
     author_email="jacob.sterling@advance.online",
